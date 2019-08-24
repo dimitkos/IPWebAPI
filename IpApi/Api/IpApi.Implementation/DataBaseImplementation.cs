ade7efef828586237ba90aef35e80f2a4f07c145
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-24 15:34:32 +0300

    add nuger to implementation and implement method to get connection string from appsetiings

---
+++ IpApi/Api/IpApi.Implementation/DataBaseImplementation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpApi.Implementation
+{
+    public class DataBaseImplementation
+    {
+        private SqlConnection GetSqlConnection()
+        {
+            var connection = new SqlConnection(ConfigurationManager.AppSettings["myConnectionString"]);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            return connection;
+        }
+
+
+    }
+}
