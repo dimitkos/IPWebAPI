88840bf675930c1089194a5e114eef9529c666ae
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-24 16:01:58 +0300

    add class getiprequest

---
+++ IpApi/Api/IpApi.Types/Request/GetIpRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpApi.Types.Request
+{
+    [DataContract]
+    public class GetIpRequest
+    {
+        [DataMember(Name = "ip")]
+        public string Ip { get; set; }
+    }
+}
