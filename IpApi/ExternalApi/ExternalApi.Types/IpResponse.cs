0a67198ff00777b7a6bb6ce099441939f5ebc9b4
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-24 14:07:56 +0300

    Add class library with types

---
+++ IpApi/ExternalApi/ExternalApi.Types/IpResponse.cs
@@ -0,0 +1,18 @@
+﻿using ExternalApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalApi.Types
+{
+    public class IpResponse : IPDetails
+    {
+        public string City { get ; set; }
+        public string Country { get; set; }
+        public string Continent { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
