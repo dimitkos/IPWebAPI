cc1b74cf9ccf72892f896a3d3237445c8a64a361
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-24 14:22:30 +0300

    Add library for exceptions

---
+++ IpApi/ExternalApi/ExternalApi.Exceptions/IPServiceNotAvailableException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalApi.Exceptions
+{
+    public class IPServiceNotAvailableException : Exception
+    {
+        public IPServiceNotAvailableException()
+        {
+
+        }
+
+        public IPServiceNotAvailableException(string message)
+        : base(String.Format("IP Service Not Available: {0}", message))
+        {
+
+        }
+    }
+}
--- IpApi/ExternalApi/ExternalApi.Implementation/IpImplementation.cs
+++ IpApi/ExternalApi/ExternalApi.Implementation/IpImplementation.cs
@@ -1,4 +1,5 @@
 ﻿using ExternalApi.Interfaces;
+using ExternalApi.Types;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -8,14 +9,22 @@ using System.Threading.Tasks;
 
 namespace ExternalApi.Implementation
 {
-    //public class IpImplementation : IIPInfoProvider
-    //{
-    //    public IPDetails GetDetails(string ip)
-    //    {
-    //        var client = new RestClient("http://api.ipstack.com");
-    //        var request = new RestRequest("/{ip} ? access_key =f6a6d50dea98aaf41a1c2fd8e90974fb", Method.GET);
-    //        //var queryResult = client.Get<IPDetails>(request).Data;
-    //        return queryResult;
-    //    }
-    //}
+    public class IpImplementation : IIPInfoProvider
+    {
+        public IPDetails GetDetails(string ip)
+        {
+            try
+            {
+                var client = new RestClient("http://api.ipstack.com");
+                var request = new RestRequest("/{ip} ? access_key =f6a6d50dea98aaf41a1c2fd8e90974fb", Method.GET);
+                var queryResult = client.Get<IpResponse>(request).Data;
+                return queryResult;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("IPServiceNotAvailableException");
+            }
+            
+        }
+    }
 }
