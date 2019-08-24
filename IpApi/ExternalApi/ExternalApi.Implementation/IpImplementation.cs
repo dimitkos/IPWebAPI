c98426800c1ef05bdf79123db105fff318c7615e
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-24 12:05:06 +0300

    add implementation class

---
+++ IpApi/ExternalApi/ExternalApi.Implementation/IpImplementation.cs
@@ -0,0 +1,21 @@
+﻿using ExternalApi.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalApi.Implementation
+{
+    //public class IpImplementation : IIPInfoProvider
+    //{
+    //    public IPDetails GetDetails(string ip)
+    //    {
+    //        var client = new RestClient("http://api.ipstack.com");
+    //        var request = new RestRequest("/{ip} ? access_key =f6a6d50dea98aaf41a1c2fd8e90974fb", Method.GET);
+    //        //var queryResult = client.Get<IPDetails>(request).Data;
+    //        return queryResult;
+    //    }
+    //}
+}
