4808162376cc14c55ff6ae7bc876472c7f186dcb
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-25 15:14:21 +0300

    implement end point

--- IpApi/Api/IpApi.Controllers/IpDetailsController.cs
+++ IpApi/Api/IpApi.Controllers/IpDetailsController.cs
@@ -1,7 +1,11 @@
 ﻿using IpApi.Interfaces;
+using IpApi.Types.Request;
+using IpApi.Types.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -15,6 +19,27 @@ namespace IpApi.Controllers
         public IpDetailsController(IMainIpService mainService)
         {
             this.mainService = mainService;
+        }
+
+        [HttpGet]
+        [ActionName("getIp")]
+        public HttpResponseMessage GetIp([FromBody] GetIpRequest ipRequest)
+        {
+            if(ipRequest ==null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Please give an Ip");
+            }
+
+            var response = mainService.FetchIp(ipRequest);
+
+            if (response.IpDetails != null)
+            {
+                return Request.CreateResponse<GetIpDetailsResponse>(HttpStatusCode.OK, response);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, " Ip Not Found");
+            }
         }
     }
 }
