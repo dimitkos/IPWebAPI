5f6d130304700806acc2e29507e875203f6fe260
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-08-25 15:43:37 +0300

    add unit tests

---
+++ IpApi/UnitTests/UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ExternalApi.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetIp()
+        {
+            IpImplementation service = new IpImplementation();
+
+            string requestIp = "xxx.xxx.xxx.xxx";//must add an ip
+            var response = service.GetDetails(requestIp);
+
+            
+            Xunit.Assert.NotNull(response.City);
+            Xunit.Assert.NotNull(response.Country);
+            Xunit.Assert.NotNull(response.Continent);
+            Xunit.Assert.True(response.Longitude !=0 && response.Latitude !=0);
+        }
+    }
+}
